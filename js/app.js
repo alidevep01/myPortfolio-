4d0bc3a036b76a9aca093a76fa8afddbdae86ba8
mohdali0326 <mohammadali0326@gmail.com>
2022-06-19 00:06:34 -0500

    Completed contact us, responsiveness, Animation, color switcher

---
+++ js/app.js
@@ -0,0 +1,7 @@
+/* ****************************** Typing Animation *********************************/
+var typed = new Typed(".typing", {
+    strings:["", "Aspiring Software Engineer", "Web Developer"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
